35cb5bb097bd3e852954dd37314735d1224030fc
Zack-K <gustokong.halohalo@gmail.com>
2020-02-17 03:57:12 +0900

    add ITP1_2_swerA an

---
+++ AOJ_Introduction_to_programming_C/small_large_equal.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void){
+    int first_arg;
+    int secound_arg;
+    char *result;
+    
+    scanf("%d %d", &first_arg, &secound_arg);
+    if (first_arg == secound_arg)
+        result = "a == b";
+    else if(first_arg < secound_arg)
+        result = "a < b";
+    else
+        result = "a > b";
+    printf("%s\n",result);
+}
