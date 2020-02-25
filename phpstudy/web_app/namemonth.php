d207f928356b5a8e756d23e8faef7a716fbfae90
Zack-K <gustokong.halohalo@gmail.com>
2020-02-25 23:09:34 +0900

    update modify html format

--- phpstudy/web_app/namemonth.php
+++ phpstudy/web_app/namemonth.php
@@ -1,14 +1,15 @@
-<!DOCTYPR html>
-<head>
-    <title>My Second Web App</title>
-</head>
-<body>
-    <h1>My Second Web App</h1>
-    <!-- form and action-->
-    <form action="action.php" method="post">
-        名前：<input type="text" name="name" />
-        生まれ月：<input type="text" name="month" />
-        <input type="submit">
-    </form>
-</body>
-
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>My Second Web App</title>
+    </head>
+    <body>
+        <h1>My Second Web App</h1>
+        <!-- form and action-->
+        <form action="action.php" method="post">
+            名前：<input type="text" name="name" />
+            生まれ月：<input type="text" name="month" />
+            <input type="submit">
+        </form>
+    </body>
+</html>
