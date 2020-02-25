e0ea4ed0df52256b8d74892d7ec7c78cf27379c1
Zack-K <gustokong.halohalo@gmail.com>
2020-02-25 20:59:44 +0900

    php basic review

---
+++ phpstudy/geammer/hello.php
@@ -0,0 +1,16 @@
+<!DOCTYPR html>
+<head>
+    <title>My First Web App</title>
+</head>
+<body>
+    <h1>My First Web App</h1>
+    <?php
+    //文字出力
+    echo"Hwllo World!\n";
+    ?>
+    <br>
+    <?php
+    echo"It runs on web server!!";
+    ?>
+</body>
+
---
+++ phpstudy/web_app/namemonth.php
@@ -0,0 +1,14 @@
+<!DOCTYPR html>
+<head>
+    <title>My Second Web App</title>
+</head>
+<body>
+    <h1>My Second Web App</h1>
+    <!-- form and action-->
+    <form action="action.php" method="post">
+        名前：<input type="text" name="name" />
+        生まれ月：<input type="text" name="month" />
+        <input type="submit">
+    </form>
+</body>
+
