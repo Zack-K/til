db3c2c9104cb46a425210fdcb71e59a698c16d69
Zack-K <gustokong.halohalo@gmail.com>
2020-03-04 04:32:46 +0900

    update modified route page that adds @endsection, web.app for blank register's error

--- phpstudy/laravelstudy/project/bookapp/routes/web.php
+++ phpstudy/laravelstudy/project/bookapp/routes/web.php
@@ -18,3 +18,21 @@ Route::get('/', function () {
     $books = Book::all();
     return view('books', [ 'books' => $books ]);
 });
+
+Route::post('/book', function(Request $request) {
+    $validator = Validator::make($request->all(),[
+        'name' => 'required|max:255'
+    ]);
+
+    if($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $book = new Book;
+    $book->title = $request->name;
+    $book->save();
+
+    return redirect('/');
+});
