34dfc25f30562b497fdc9560c84c7e5efdb13f03
Zack-K <gustokong.halohalo@gmail.com>
2022-04-07 04:43:06 +0900

    Update section-9 done!

---
+++ ruby-book/lib/section-9/regexp_checker.rb
@@ -0,0 +1,18 @@
+print 'test?: '
+text = gets.chomp
+
+begin
+  print 'Pattern?: '
+  pattern = gets.chomp
+  regexp = Regexp.new(pattern)
+rescue RegexpError => e 
+  puts "Invalid patten: #{e.message}"
+  retry
+end
+
+matches = text.scan(regexp)
+if matches.size > 0
+  puts "Matched; #{matches.join(', ')}"
+else
+  puts 'Nothing mathched.'
+end
